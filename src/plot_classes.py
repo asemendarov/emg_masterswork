bf88d070efdbaeb51b0ea1f73b93e1e0ad043539
oNoComments <31689842+oNoComments@users.noreply.github.com>
2019-04-25 21:04:06 +0300

    Version 1.1.1: Create plot_classes.py

---
+++ src/plot_classes.py
@@ -0,0 +1,52 @@
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import src.main_classes_search as cl_search
+
+# global variable
+begin_class = 9
+end_class = 10
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
+ax1, ax2 = ax.flatten()
+ax1.set_title('Train Result')
+ax2.set_title('Test Result')
+# ax.set_xlabel('')
+# ax.set_ylabel('')
+
+
+def plot(train_test_result):
+    train_result_list, test_result_list = [(result[1][1], result[1][0]) for result in train_test_result]
+
+    ax1.plot(train_result_list, 'ro-')
+    ax2.plot(test_result_list, 'ro-')
+
+    plt.grid()
+    plt.show()
+
+
+def main():
+    cl_search.classes = '0, 1, 2, 3, 4, 5, 6, 7, 8, 9'
+
+    start_time = time.time()
+
+    result = list()
+
+    for combinations_len in range(begin_class, end_class + 1):
+        print(f'Start ({combinations_len}/{end_class})')
+
+        cl_search.combinations_len = combinations_len
+        out_search = cl_search.main(print_result=False)
+        result.append(out_search[-1])
+
+        print(f'Stop ({combinations_len}/{end_class}). Time: {time.time() - start_time} seconds')
+
+    # result = sorted(result, key=lambda kv: kv[1])
+
+    print('\n', np.array(result))
+
+    plot(result)
+
+
+if __name__ == '__main__':
+    main()
